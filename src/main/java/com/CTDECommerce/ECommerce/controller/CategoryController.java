4814de99e54126bcbce92fe3c5c52a9031524145
Silvano AP <silvanoeng@hotmail.com>
2021-12-05 00:06:43 -0300

    liberando porta 3000

--- src/main/java/com/CTDECommerce/ECommerce/controller/CategoryController.java
+++ src/main/java/com/CTDECommerce/ECommerce/controller/CategoryController.java
@@ -15,7 +15,7 @@ public class CategoryController {
     @Autowired
     private CategoryServiceImpl categoryService;
 
-
+    @CrossOrigin(origins = "http://localhost:3000)
     @PostMapping
     public ResponseEntity<CategoryDTO> salvar(@RequestBody CategoryDTO category) {
         List<CategoryDTO> categoryDTOList = categoryService.buscarTodos();
@@ -30,22 +30,26 @@ public class CategoryController {
         return ResponseEntity.ok(categoryDTO1);
     }
 
+    @CrossOrigin(origins = "http://localhost:3000)
     @GetMapping("/buscartodos")
     public ResponseEntity<List<CategoryDTO>>findAll(){
         return ResponseEntity.ok(categoryService.buscarTodos());
     }
 
+    @CrossOrigin(origins = "http://localhost:3000)
     @GetMapping("/buscartodosname")
     public ResponseEntity<List<String>>findAllName(){
         return ResponseEntity.ok(categoryService.buscarTodosString());
     }
 
+    @CrossOrigin(origins = "http://localhost:3000)
     @GetMapping("/id/{id}")
     public ResponseEntity buscarPorId(@PathVariable Long id) {
         CategoryDTO categoryDTO = categoryService.buscarPorId(id);
         return ResponseEntity.ok(categoryDTO);
     }
 
+    @CrossOrigin(origins = "http://localhost:3000)
     @GetMapping("/name/{name}")
     public ResponseEntity buscarPorName(@PathVariable String name) {
         CategoryDTO categoryDTO = categoryService.buscarPorNome(name);
